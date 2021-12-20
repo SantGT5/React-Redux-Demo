b5277f23f5c746529e5f02f73aa1eb433e29e3b5
SantGT5 <gianspf@gmail.com>
2021-12-20 16:31:44 +0100

    Payload config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import './App.css';
-import CakeContainer from './components/CakeContainer';
+import "./App.css";
+import CakeContainer from "./components/CakeContainer";
 
 // Redux
-import { Provider } from "react-redux"
-import store from './redux/store';
-import HooksCakeContainer from './components/HooksCakeContainer';
-import IceCreamContainer from './components/IceCreamContainer';
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import HooksCakeContainer from "./components/HooksCakeContainer";
+import IceCreamContainer from "./components/IceCreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 
 function App() {
-
-  console.log(store.getState())
+  console.log(store.getState());
 
   return (
     <Provider store={store}>
-    <div className="App">
-    <HooksCakeContainer />
-    <CakeContainer />
-    <IceCreamContainer />
-    </div>
+      <div className="App">
+      <ItemContainer cake/>
+      <ItemContainer />
+        <HooksCakeContainer />
+        <CakeContainer />
+        <IceCreamContainer />
+        <NewCakeContainer />
+      </div>
     </Provider>
   );
 }
